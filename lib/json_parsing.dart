604d9eb30ed81963ec4c8f32296e7d7ec58c4aa1
lijianan <574980606@qq.com>
2026-07-27 17:55:54 +0800

    update

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import './data_model.dart';
 
 class JsonParsingPage extends StatefulWidget {
   const JsonParsingPage({super.key});
@@ -15,7 +16,7 @@ class _JsonParsingPageState extends State<JsonParsingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('JSON Parsing 应用实战')),
-      body: Column(children: [_json2MapBtn(), Text('解析的结果：$resultShow')]),
+      body: Column(children: [_json2MapBtn(), _json2ModelBtn(), Text('解析的结果：$resultShow')]),
     );
   }
 
@@ -23,11 +24,25 @@ class _JsonParsingPageState extends State<JsonParsingPage> {
     return ElevatedButton(onPressed: _json2MapClick, child: const Text('JSON 转 Map'));
   }
 
+  Widget _json2ModelBtn() {
+    return ElevatedButton(onPressed: _json2ModelClick, child: const Text('JSON 转 Model'));
+  }
+
   void _json2MapClick() {
     const jsonString = '{"name": "张三", "age": 18, "gender": "男"}';
     Map<String, dynamic> map = jsonDecode(jsonString);
     setState(() {
       resultShow = '姓名：${map['name']}, 年龄：${map['age']}, 性别：${map['gender']}';
+    });
+  }
+
+  void _json2ModelClick() {
+    var jsonString =
+        '{ "code": 1, "message": "SUCCESS", "data": { "time": 0, "method": "get", "requestId": "1687331550" } }';
+    Map<String, dynamic> map = jsonDecode(jsonString);
+    DataModel data = DataModel.fromJson(map);
+    setState(() {
+      resultShow = 'time: ${data.time}, method: ${data.method}, requestId: ${data.requestId}';
     });
   }
 }
