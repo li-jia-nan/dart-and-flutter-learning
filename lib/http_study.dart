3989e269bbd2d60c8077c4843b6385d8f09a890c
lijianan <574980606@qq.com>
2026-07-27 11:50:04 +0800

    update

--- lib/http_study.dart
+++ lib/http_study.dart
@@ -9,34 +9,65 @@ class HttpStudy extends StatefulWidget {
 }
 
 class _HttpStudyState extends State<HttpStudy> {
+  var responseData = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('基于 Http 实现网络操作 - Flutter 网络开发实战应用')),
-      body: Column(children: [_doGetBtn()]),
+      body: Column(
+        children: [
+          _doGetBtn(),
+          _doPostBtn(),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: SelectableText('Http Get 请求结果：$responseData'),
+            ),
+          ),
+        ],
+      ),
     );
   }
-}
 
-Widget _doGetBtn() {
-  return ElevatedButton(onPressed: _doGetClick, child: const Text('发送 Http Get 请求'));
-}
+  Widget _doGetBtn() {
+    return ElevatedButton(onPressed: _doGetClick, child: const Text('发送 Http Get 请求'));
+  }
 
-// 发送 Http Get 请求
-Future<void> _doGetClick() async {
-  try {
+  Widget _doPostBtn() {
+    return ElevatedButton(onPressed: _doPostClick, child: const Text('发送 Http Post 请求'));
+  }
+
+  // 发送 Http Get 请求
+  Future<void> _doGetClick() async {
     final url = Uri.parse('https://vercel.com/design.md');
     final response = await http.get(url);
-
-    debugPrint('请求 response: $response');
-
     if (response.statusCode == 200) {
       debugPrint('请求成功，响应数据：${response.body}');
+      setState(() {
+        responseData = response.body;
+      });
     } else {
-      debugPrint('请求失败，状态码：${response.statusCode}');
+      setState(() {
+        responseData = '请求失败：${response.body}';
+      });
     }
-  } catch (error, stackTrace) {
-    debugPrint('请求异常：$error');
-    debugPrintStack(stackTrace: stackTrace);
+  }
+
+  // 发送 Http Post 请求
+  Future<void> _doPostClick() async {
+    final uri = Uri.parse('https://postman-echo.com/post');
+    const params = {'key': 'value'}; // 这里可以根据实际需求设置请求参数
+    final response = await http.post(uri, body: params);
+    if (response.statusCode == 200) {
+      debugPrint('POST 请求成功，响应数据：${response.body}');
+      setState(() {
+        responseData = response.body;
+      });
+    } else {
+      setState(() {
+        responseData = 'POST 请求失败：${response.body}';
+      });
+    }
   }
 }
