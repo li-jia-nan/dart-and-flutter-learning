c289eaf009c010ce9009e907a29c0c7bd12b2eda
lijianan <574980606@qq.com>
2026-07-28 11:59:17 +0800

    update

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_app/utils/view_util.dart';
 
 // 登录页
 class LoginPage extends StatefulWidget {
@@ -15,10 +16,29 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Stack(children: [..._background()]));
+    return Scaffold(body: Stack(children: [..._background(), _content()]));
   }
 
-  _background() {
-    return [];
+  List<Widget> _background() {
+    return [
+      Positioned.fill(child: Image.asset('images/result-modal-bg.webp', fit: BoxFit.cover)),
+      Positioned.fill(
+        child: Container(decoration: const BoxDecoration(color: Colors.black54)),
+      ),
+    ];
+  }
+
+  Widget _content() {
+    return Positioned.fill(
+      left: 25,
+      right: 25,
+      child: ListView(
+        children: [
+          hiSpace(height: 100),
+          const Text('账号密码登录', style: TextStyle(fontSize: 26, color: Colors.white)),
+          hiSpace(height: 40),
+        ],
+      ),
+    );
   }
 }
