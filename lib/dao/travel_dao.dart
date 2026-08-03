321e39376d33dacaa9eef898f34625582e26918e
lijianan <574980606@qq.com>
2026-08-03 10:08:39 +0800

    update

--- lib/dao/travel_dao.dart
+++ lib/dao/travel_dao.dart
@@ -4,12 +4,14 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:learning_app/dao/header_util.dart';
 import 'package:learning_app/model/travel_category_model.dart';
+import 'package:learning_app/model/travel_tab_model.dart';
 import 'package:learning_app/utils/navigator_util.dart';
 
 class TravelDao {
-  static const String _mockDataAsset = 'lib/model/travel-category-data.json';
+  static const String _mockCategoryData = 'lib/model/travel-category-data.json';
+  static const String _mockTabData = 'lib/model/travel-tab-data.json';
 
-  // 获取旅拍类别列表
+  // 获取旅拍类别
   static Future<TravelCategoryModel?> getCategory() async {
     var url = Uri.parse('https://api.devio.org/uapi/travel/list');
     final response = await http.get(url, headers: hiHeaders());
@@ -21,23 +23,64 @@ class TravelDao {
     }
 
     if (response.statusCode == 200) {
-      final data = _parseData(responseBody);
+      final data = _parseCategoryData(responseBody);
       if (data != null) {
         return data;
       }
     }
 
     // 接口失败或响应中没有 data 时，使用本地模拟数据。
-    final mockResponseBody = await rootBundle.loadString(_mockDataAsset);
-    return _parseData(mockResponseBody);
+    final mockResponseBody = await rootBundle.loadString(_mockCategoryData);
+    return _parseCategoryData(mockResponseBody);
   }
 
-  static TravelCategoryModel? _parseData(String responseBody) {
+  // 获取旅拍类别下的旅拍列表
+  static Future<TravelTabModel?> getTravels(
+    String groupChannelCode,
+    int pageIndex,
+    int pageSize,
+  ) async {
+    Map<String, String> paramsMap = {
+      'pageIndex': pageIndex.toString(),
+      'pageSize': pageSize.toString(),
+      'groupChannelCode': groupChannelCode,
+    };
+    var url = Uri.https('api.devio.org', '/uapi/travel/list', paramsMap);
+    final response = await http.get(url, headers: hiHeaders());
+    Utf8Decoder utf8Decoder = const Utf8Decoder();
+    String responseBody = utf8Decoder.convert(response.bodyBytes);
+    if (response.statusCode == 401) {
+      NavigatorUtil.goToLogin();
+      return null;
+    }
+
+    if (response.statusCode == 200) {
+      final data = _parseTabData(responseBody);
+      if (data != null) {
+        return data;
+      }
+    }
+
+    // 接口失败或响应中没有 data 时，使用本地模拟数据。
+    final mockResponseBody = await rootBundle.loadString(_mockTabData);
+    return _parseTabData(mockResponseBody);
+  }
+
+  static TravelCategoryModel? _parseCategoryData(String responseBody) {
     final result = jsonDecode(responseBody);
     if (result is! Map<String, dynamic>) {
       return null;
     }
     TravelCategoryModel dataModel = TravelCategoryModel.fromJson(result);
+    return dataModel;
+  }
+
+  static TravelTabModel? _parseTabData(String responseBody) {
+    final result = jsonDecode(responseBody);
+    if (result is! Map<String, dynamic>) {
+      return null;
+    }
+    TravelTabModel dataModel = TravelTabModel.fromJson(result);
     return dataModel;
   }
 }
--- lib/model/travel_tab_model.dart
+++ lib/model/travel_tab_model.dart
@@ -1,1 +1,38 @@
-//
+class TravelTabModel {
+  final int code;
+  final String message;
+  final Data data;
+
+  TravelTabModel({required this.code, required this.message, required this.data});
+
+  factory TravelTabModel.fromJson(Map<String, dynamic> json) => TravelTabModel(
+    code: json["code"],
+    message: json["message"],
+    data: Data.fromJson(json["data"]),
+  );
+
+  Map<String, dynamic> toJson() => {"code": code, "message": message, "data": data.toJson()};
+}
+
+class Data {
+  final List<TravelTab> tabs;
+
+  Data({required this.tabs});
+
+  factory Data.fromJson(Map<String, dynamic> json) =>
+      Data(tabs: List<TravelTab>.from(json["tabs"].map((x) => TravelTab.fromJson(x))));
+
+  Map<String, dynamic> toJson() => {"tabs": List<dynamic>.from(tabs.map((x) => x.toJson()))};
+}
+
+class TravelTab {
+  final String labelName;
+  final String groupChannelCode;
+
+  TravelTab({required this.labelName, required this.groupChannelCode});
+
+  factory TravelTab.fromJson(Map<String, dynamic> json) =>
+      TravelTab(labelName: json["labelName"], groupChannelCode: json["groupChannelCode"]);
+
+  Map<String, dynamic> toJson() => {"labelName": labelName, "groupChannelCode": groupChannelCode};
+}
