3aaa900777d9b89c5314c25c7673af63b95ef6b8
lijianan <574980606@qq.com>
2026-08-07 14:40:42 +0800

    update

---
+++ lib/base/js2flutter_by_url.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+const String h5JS2FlutterByUrl = '''
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <button id="btn" style="font-size: 16px">传递参数</button>
+    <script>
+      const btn = document.getElementById("btn");
+      btn.addEventListener(
+        "click",
+        () => {
+          // 通过 URL 传递参数给 Flutter
+          document.location = "hi://webview?name=张三&age=18";
+        },
+        false,
+      );
+    </script>
+  </body>
+</html>
+''';
+
+class Js2flutterByUrl extends StatefulWidget {
+  const Js2flutterByUrl({super.key});
+
+  @override
+  State<Js2flutterByUrl> createState() => _Js2flutterByUrlState();
+}
+
+class _Js2flutterByUrlState extends State<Js2flutterByUrl> {
+  late WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) {
+            // 约定一个通信协议 hi://webview
+            if (request.url.startsWith('hi://webview')) {
+              debugPrint('处理 JS 通过 url 传递过来的数据: $request');
+              Uri uri = Uri.parse(request.url);
+              // 解析参数
+              var name = uri.queryParameters['name'];
+              debugPrint('name: $name');
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('name: $name')));
+              return NavigationDecision.prevent; // 阻止 WebView 继续加载这个 url
+            } else {
+              debugPrint('继续加载 url: $request');
+              return NavigationDecision.navigate; // 允许 WebView 继续加载这个 url
+            }
+          },
+        ),
+      );
+  }
+
+  Widget get _loadBtn => FilledButton(
+    onPressed: () {
+      _controller.loadHtmlString(h5JS2FlutterByUrl);
+    },
+    child: const Text('加载 h5 页面', style: TextStyle(color: Colors.black, fontSize: 16)),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('通过 Js 向 flutter 传递参数'), actions: [_loadBtn]),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:learning_app/dao/login_dao.dart';
-import 'package:learning_app/navigator/tab_navigator.dart';
-// import 'package:learning_app/pages/home_page.dart';
-// import 'package:learning_app/pages/expansion_tile_page.dart';
-// import 'package:learning_app/pages/gridview_page.dart';
-// import 'package:learning_app/pages/refresh_loadmore_page.dart';
-// import 'package:learning_app/pages/horizontal_list_page.dart';
-// import 'package:learning_app/pages/vertical_list_page.dart';
-import 'package:learning_app/utils/screen_adapter_helper.dart';
+import 'package:learning_app/base/js2flutter_by_url.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,25 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-      home: FutureBuilder<String?>(
-        future: LoginDao.getBoardingPass(),
-        builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-          ScreenHelper.init(context);
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.data == null) {
-              return const TabNavigator();
-            } else {
-              return const TabNavigator();
-            }
-          } else {
-            return const Scaffold(body: Center(child: CircularProgressIndicator()));
-          }
-        },
-      ),
-    );
+    return MyHomePage(title: 'Flutter Demo Home Page');
   }
 }
 
@@ -51,14 +23,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,18 +32,19 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('当前点击按钮次数：'),
-            Text('$_counter', style: const TextStyle(fontSize: 30)),
-          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[_navButton(context, const Js2flutterByUrl(), '通过 Js 向 flutter 传递参数')],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+
+  Widget _navButton(BuildContext context, Widget page, String title) {
+    return FilledButton(
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+      },
+      child: Text(title),
     );
   }
 }
