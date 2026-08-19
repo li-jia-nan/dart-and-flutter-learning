1de2eaf879a8d3732cb00a16785e87d7551d30dc
lijianan <574980606@qq.com>
2026-08-19 10:13:43 +0800

    update

--- lib/mvvm/binding/initialBinding.dart
+++ lib/mvvm/binding/initialBinding.dart
@@ -1,10 +1,12 @@
 // 工程级别的绑定
 import 'package:get/get.dart';
+import 'package:learning_app/mvvm/login/bindings/main_binding.dart';
 import 'package:learning_app/mvvm/main/binding/main_binding.dart';
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
     MainBinding().dependencies();
+    LoginBinding().dependencies();
   }
 }
